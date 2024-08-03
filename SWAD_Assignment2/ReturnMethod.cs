1ce9aefc1be87f5d69e05e2cd6880c131788abfb
weiyingeve <s10257093@connect.np.edu.sg>
2024-08-03 17:04:58 +0800

    updated return classes

--- SWAD_Assignment2/ReturnMethod.cs
+++ SWAD_Assignment2/ReturnMethod.cs
@@ -10,32 +10,4 @@ namespace SWAD_Assignment2
     {
         public ReturnMethod() { }
     }
-    class SelfReturn : ReturnMethod 
-    {
-        private DateTime dateTimeReturn;
-        private string iCarReturnLocation;
-        private AdditionalCharge additionalCharge;
-        public DateTime DateTimeReturn
-        {
-            get { return dateTimeReturn; }
-            set { dateTimeReturn = value; }
-        }
-        public string ICarReturnLocation
-        {
-            get { return iCarReturnLocation;}
-            set { iCarReturnLocation = value; }
-        }
-        public AdditionalCharge AdditionalCharge
-        {
-            get { return additionalCharge; }
-            set { additionalCharge = value; }
-        }
-        public SelfReturn() { }
-        public SelfReturn(DateTime dateTimeReturn, string iCarReturnLocation, AdditionalCharge additionalCharge)
-        {
-            this.dateTimeReturn = dateTimeReturn;
-            this.iCarReturnLocation = iCarReturnLocation;
-            AdditionalCharge=additionalCharge;
-        }
-    }
 }
---
+++ SWAD_Assignment2/SelfReturn.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWAD_Assignment2
+{
+    class SelfReturn : ReturnMethod
+    {
+        private DateTime dateTimeReturn;
+        private string iCarReturnLocation;
+        private AdditionalCharge additionalCharge;
+        public DateTime DateTimeReturn
+        {
+            get { return dateTimeReturn; }
+            set { dateTimeReturn = value; }
+        }
+        public string ICarReturnLocation
+        {
+            get { return iCarReturnLocation; }
+            set { iCarReturnLocation = value; }
+        }
+        public AdditionalCharge AdditionalCharge
+        {
+            get { return additionalCharge; }
+            set { additionalCharge = value; }
+        }
+        public SelfReturn() { }
+        public SelfReturn(DateTime dateTimeReturn, string iCarReturnLocation, AdditionalCharge additionalCharge)
+        {
+            this.dateTimeReturn = dateTimeReturn;
+            this.iCarReturnLocation = iCarReturnLocation;
+            AdditionalCharge=additionalCharge;
+        }
+    }
+}
