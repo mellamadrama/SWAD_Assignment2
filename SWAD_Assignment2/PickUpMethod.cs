43f748a3c981af6e469debc6dc6471af2c1c7bab
weiyingeve <s10257093@connect.np.edu.sg>
2024-08-04 18:18:51 +0800

    reverted pickupmethod and returnmethod classes

--- SWAD_Assignment2/PickUpMethod.cs
+++ SWAD_Assignment2/PickUpMethod.cs
@@ -6,30 +6,9 @@ using System.Threading.Tasks;
 
 namespace SWAD_Assignment2
 {
-    internal class PickupMethod
+    abstract class PickUpMethod
     {
-        private Pickup pickup;
-        private DeliverCar deliverCar;
-
-        public Pickup Pickup
-        {
-            get { return pickup; }
-            set { pickup = value; }
-        }
-
-        public DeliverCar DeliverCar
-        {
-            get { return deliverCar; }
-            set { deliverCar = value; }
-        }
-
-        public PickupMethod() { }
-
-        public PickupMethod(Pickup pickup, DeliverCar deliverCar)
-        {
-            this.pickup = pickup;
-            this.deliverCar = deliverCar;
-        }
+        public PickUpMethod() { }
     }
 
 }
--- SWAD_Assignment2/ReturnMethod.cs
+++ SWAD_Assignment2/ReturnMethod.cs
@@ -6,30 +6,9 @@ using System.Threading.Tasks;
 
 namespace SWAD_Assignment2
 {
-    internal class ReturnMethod
+    abstract class ReturnMethod
     {
-        private SelfReturn selfReturn;
-        private DeliveryReturn deliveryReturn;
-
-        public SelfReturn SelfReturn
-        {
-            get { return selfReturn; }
-            set { selfReturn = value; }
-        }
-
-        public DeliveryReturn DeliveryReturn
-        {
-            get { return deliveryReturn; }
-            set { deliveryReturn = value; }
-        }
-
         public ReturnMethod() { }
-
-        public ReturnMethod(SelfReturn selfReturn, DeliveryReturn deliveryReturn)
-        {
-            this.selfReturn = selfReturn;
-            this.deliveryReturn = deliveryReturn;
-        }
     }
 
 }
