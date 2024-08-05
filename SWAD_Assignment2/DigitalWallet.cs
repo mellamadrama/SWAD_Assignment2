d0be75afa4c99a483e25dfb9248a91e8028c7923
sudarsanamrithika <veethe2nd@gmail.com>
2024-08-05 16:27:37 +0800

    add new cs files for payment methods + add more to additional charge

---
+++ SWAD_Assignment2/DigitalWallet.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWAD_Assignment2
+{
+    internal class DigitalWallet : PaymentMethod
+    {
+        public string Type { get; set; }
+
+        public DigitalWallet() { }
+
+        public DigitalWallet(string type)
+        {
+            Type = type;
+        }
+    }
+}
